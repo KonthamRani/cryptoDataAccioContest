3fc008b4dffa69b3ca43305300514e58fe757341
Rani Kontham <100075436+KonthamRani@users.noreply.github.com>
2022-11-29 23:13:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,20 +12,19 @@ fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
         let num3=new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(values.market_cap);
         console.log(num1);
         
-         tableInsert+=`<tr>
+         tableInsert+=`<tr style="border-bottom:2px solid white">
         <td> <img src="${values.image}" height="30px" width="30px"><span style="padding-left:10px;">${values.id}</span></td>
         <td>${values.symbol.toUpperCase()}</td>
         <td>${num1}</td>
         <td>${num2}</td>
-        <td>${parseFloat(values.market_cap_change_24h).toFixed(2)}%</td>
-        <td></td>
-        <td></td>
-       
-        <tr>
+        <td id="green" style="${values.market_cap_change_percentage_24h>0 ? "color:green" : "color:red"}">${values.market_cap_change_percentage_24h.toFixed(2)}%</td>
+        <td>Mkt Cap: ${num3}</td> 
+        </tr>
         `;
        
    
     })
+    
     document.getElementById("_tbody").innerHTML=tableInsert;
 })
         
