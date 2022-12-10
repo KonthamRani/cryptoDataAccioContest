d0385a8da4e19d9803b1d802fc1bfe2dedeeb4b9
Rani Kontham <100075436+KonthamRani@users.noreply.github.com>
2022-12-10 22:52:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 
-let tableInsert="";
-fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
-.then((data)=>{
-   return  data.json();
-}).then((objData)=>{
-    objData.map(values=>{
-        let num1=new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(values.current_price);
-        console.log(num1);
-        let num2=new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(values.market_cap_change_24h);
-        console.log(num1);
-        let num3=new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(values.market_cap);
-        console.log(num1);
-        
-         tableInsert+=`<tr style="border-bottom:2px solid white">
-        <td> <img src="${values.image}" height="30px" width="30px"><span style="padding-left:10px;">${values.id}</span></td>
-        <td>${values.symbol.toUpperCase()}</td>
-        <td>${num1}</td>
-        <td>${num2}</td>
-        <td id="green" style="${values.market_cap_change_percentage_24h>0 ? "color:green" : "color:red"}">${values.market_cap_change_percentage_24h.toFixed(2)}%</td>
-        <td>Mkt Cap: ${num3}</td> 
-        </tr>
-        `;
-       
-   
+
+async function getData() {
+    let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+    let result = await fetch(url);
+    let data = result.json()
+    return data
+}
+let tableData = ""
+getData()
+    .then(res => {
+        console.log(res)
+        res.map(data => {
+            let num1=new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(data.current_price);
+            let num2=new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(data.market_cap_change_24h);
+            let num3=new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(data.market_cap);
+            
+            tableData += `
+            
+            <tr>
+                <td style="display:flex;align-items:center"><img src="${data.image}" height="30px" width="30px"><span style="margin-left:1rem;">${data.name}</span></td>
+                <td>${data.symbol.toUpperCase()}</td>
+                <td>${num1}</td>
+                <td>${num2}</td>
+                <td style="${data.market_cap_change_percentage_24h>0?"color:green":"color:red"}">${data.market_cap_change_percentage_24h.toFixed(2)}%</td>
+                <td>Mkt Cap : ${num3}</td>
+                </tr>
+
+            
+            `
+        })
+        document.getElementById("tBody").innerHTML = tableData;
     })
-    
-    document.getElementById("_tbody").innerHTML=tableInsert;
-})
-        
-    
